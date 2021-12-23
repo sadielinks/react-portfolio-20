abd6587bc4321ec7d0adf6b083d1d2000401f89e
Sadie Sial <sadiesial@gmail.com>
2021-12-23 11:40:49 -0600

    header+footer imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hellllooooooo world
+          hellllooooooo world!
         </p>
       </header>
     </div>
