083900eb07533401cd4a2c643c99a77c30206103
Sadie Sial <sadiesial@gmail.com>
2021-12-24 13:32:14 -0600

    added basic HTML from prev. portfolio

---
+++ src/sections/skills.js
@@ -0,0 +1,32 @@
+// copied from OG HTML
+
+// <div class="skills-icon">
+//         <div class="first-set animate__animated animate__pulse">
+
+//           <!-- HTML -->
+//           <img src="https://img.icons8.com/ios-filled/50/000000/html-5--v1.png" alt="HTML 5" />
+
+//           <!-- JS -->
+//           <img src="https://img.icons8.com/ios-filled/50/000000/javascript.png" alt="Javascript ES2015" />
+
+//           <!-- CSS -->
+//           <img src="https://img.icons8.com/ios-filled/60/000000/css3.png" alt="CSS 3" />
+
+//           <!-- GIT -->
+//           <img src="https://img.icons8.com/ios-glyphs/62/000000/github.png" alt="GitHub" />
+
+//           <!-- SQL -->
+//           <img src="https://img.icons8.com/ios/75/000000/mysql-logo.png" alt="MySQL" />
+
+//           <!-- Postgress -->
+//           <img src="https://img.icons8.com/ios-filled/50/000000/postgreesql.png" alt="PostgreSQL" />
+
+//           <!-- Node.JS -->
+//           <img src="https://img.icons8.com/windows/60/000000/node-js.png" alt="Node.js" />
+
+//           <!-- Heroku -->
+//           <img src="https://img.icons8.com/ios-filled/48/000000/heroku.png" alt="Heroku Deployment" />
+
+
+//         </div>
+//       </div>
