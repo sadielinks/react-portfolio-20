26f57f6016a5845e71bbf65869b5649b0692fc9a
Sadie Sial <sadiesial@gmail.com>
2021-12-25 13:31:32 -0600

    components + sections imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+// components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+// sections
+import About from './sections/about'
+import Skills from './sections/skills'
+import Portfolio from './sections/portfolio'
+import Contact from './sections/contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>
           hellllooooooo world!
         </p>
