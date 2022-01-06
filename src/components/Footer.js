0c4680b314bd2092d41e3cf9bf4555a555ea4141
Sadie Sial <sadiesial@gmail.com>
2022-01-06 00:45:17 -0600

    added in prev footer code from last portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,25 @@ function Footer({ text, bgColor, textColor }) {
     }
 
     return (
-        <footer style={footerStyles}>
-            <div className="container">
-                <h2>{text}</h2>
+        <div className="footer" id="footer">
+            <div id="footer-icons">
+                {/* Phone */}
+                <a href="tel:(920)-368-6336" target="_blank"><img src="https://img.icons8.com/ios-filled/50/000000/apple-phone.png" /></a>
+                {/* Email */}
+                <a href="mailto:sadiecodes@gmail.com" target="_blank"><img
+                    src="https://img.icons8.com/ios-filled/50/000000/send-mass-email.png" alt='email' class='icon' /></a>
+                {/* LinkedIn */}
+                <a href="https://www.linkedin.com/in/sadie-sial-bootcamp" target="_blank"><img
+                    src="https://img.icons8.com/ios-glyphs/55/000000/linkedin.png" alt="LinkedIn" class="icon" /></a>
+                {/* GitHub */}
+                <a href="https://github.com/sadielinks" target="_blank"><img src="https://img.icons8.com/ios-glyphs/55/000000/github.png" alt="GitHub" class="icon" /></a>
             </div>
-        </footer>
+            <h5> Made with ✨&nbsp;in Chicago by Sadie Sial
+            </h5>
+
+
+
+        </div>
     )
 }
 
