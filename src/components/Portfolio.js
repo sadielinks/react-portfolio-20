9288c73e7f0f58424893fca241406e63c0caac53
Sadie Sial <sadiesial@gmail.com>
2022-01-14 16:09:07 -0600

    portfolio without json

---
+++ src/components/Portfolio.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ProjectCards from '../../components/Project'
+import portfolio from './portfolio.json'
+
+function Wrapper(props) {
+    return <div className='wrapper'>{props.children}</div>;
+}
+
+function Portfolio() {
+
+    return (
+        <section>
+            <div className='project'>
+                <h1 className='title'>My Portfolio:</h1>
+                <hr></hr>
+            </div>
+
+            {/* going to try BCS LA suggestion with JSON file! */}
+            <Wrapper id='card-data'>
+                {portfolio.map((project) => (
+                    <ProjectCards key={project.id}
+                        image={project.image}
+                        name={project.name}
+                        github={project.github}
+                        deploy={project.deploy}
+                    />
+                ))}
+            </Wrapper>
+        </section>
+
+    );
+}
+
+export default Portfolio;
