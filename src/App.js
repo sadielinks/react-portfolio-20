8c61346072256c32b2b1296a5282e5d965a759fb
Sadie Sial <sadiesial@gmail.com>
2022-01-14 16:31:46 -0600

    cleaned up app.js HEAVY bruh

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-import { useState } from 'react'
-import React from "react";
-
-// https://v5.reactrouter.com/web/guides/quick-start
-import { Route, Redirect, HashRouter } from "react-router-dom";
-import bootstrap from "react-bootstrap";
-
-// components main
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Nav from './components/Navigation';
-
-// component sub-sections
-import About from './components/about'
-// import Skills from './components/skills'
-import Portfolio from './components/Portfolio/Portfolio'
-import Contact from './components/contact'
-
-
-
-{/* <>
-<Header />
-<div>
-
-</div>
-<Route exact path="/" component={About} />
-<Route exact path="/skills" component={Skills} />
-<Route exact path="/portfolio" component={Portfolio} />
-<Route exact path="/contact" component={Contact} />
-<Footer />
-</> */}
 
 function App() {
+
   return (
-    <>
-      <Header />
-      <div className='container'>
+    <main>
+      <Header></Header>
+      <div>
+      <Footer></Footer>
       </div>
-      <Nav />
-      <Footer />
-    </>
+    </main>
   );
 }
 
