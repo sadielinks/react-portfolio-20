ade8602f3d886e7b8627b0d8dfb149d61ef68d35
Sadie Sial <sadiesial@gmail.com>
2022-01-14 16:27:57 -0600

    added in skills icons

---
+++ src/components/Skills.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Skills() {
+
+    return (
+        <section className="my-5" id="skills">
+            <h1 id="skills">My current skill set:</h1><hr></hr>
+            <div className="my-2">
+                <img src='https://img.icons8.com/ios-filled/50/000000/html-5--v1.png' className="my-2" style={{ width: "18%" }} alt='HTML 5' />
+                <img src='https://img.icons8.com/ios-filled/50/000000/javascript.png' className="my-2" style={{ width: "18%" }} alt='Javascript ES6' />
+                <img src='https://img.icons8.com/ios-filled/60/000000/css3.png' className="my-2" style={{ width: "18%" }} alt='CSS 3' />
+                <img src='https://img.icons8.com/ios-glyphs/62/000000/github.png' className="my-2" style={{ width: "18%" }} alt='GitHub' />
+                <img src='https://img.icons8.com/ios/75/000000/mysql-logo.png' className="my-2" style={{ width: "18%" }} alt='MySQL' />
+                <img src='https://img.icons8.com/ios-filled/50/000000/postgreesql.png' className="my-2" style={{ width: "18%" }} alt='PostgreSQL' />
+                <img src='https://img.icons8.com/windows/60/000000/node-js.pn' className="my-2" style={{ width: "18%" }} alt='Node.js' />
+                <img src='https://img.icons8.com/ios-filled/48/000000/heroku.png' className="my-2" style={{ width: "18%" }} alt='Heroku Deployment' />
+            </div>
+        </section>
+    )
+}
+
+export default skills
